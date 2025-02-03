bed318478ab1066335144804f75d2979b3cece7f
Yermek Orazgaliyev <yermek.orazgaliyev@smartgroups.io>
2025-02-03 10:29:27 +0500

    table view for search

--- src/components/person/SearchForm.jsx
+++ src/components/person/SearchForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import personManager from "@/services/person";
 
 export default function SearchForm() {
@@ -23,29 +23,43 @@ export default function SearchForm() {
 
     return (
         <form onSubmit={submit}>
-            <div className="row mb-3">
-                <label htmlFor="inputName" className="col-sm-2 col-form-label">Name:</label>
-                <div className="col-sm-10">
-                    <div className="d-flex gap-3 align-items-center">
-                        <input value={name} onChange={(e) => setName(e.target.value.trimStart())} type="text" className="form-control" id="inputName" required />
-                        <button className="btn btn-primary" type="submit">Search</button>
-                    </div>
-                </div>
-            </div>
-
-            <ul className="list-group">
-                {!loading && (persons.length ? persons.map(person => (
-                    <li key={person.id} className="list-group-item d-flex align-center justify-content-between"><span>{person.name}</span><span>{formatDate(person?.dob)}</span></li>
-                )) : <div className="py-5 text-center text-secondary nothing-found">Nothing found</div>)}
-            </ul>
-
-            {loading && <div className="loading d-flex align-items-center justify-content-center rounded py-5">
-                <div className="text-center">
-                    <div>
-                        <div className="spinner-border text-primary"></div>
-                    </div>
-                </div>
-            </div>}
+            <table className="w-100 table-bordered">
+                <thead>
+                    <tr>
+                        <th className="p-2">
+                            Name:
+                        </th>
+                        <th>
+                            <input value={name} onChange={(e) => setName(e.target.value.trimStart())} type="text" className="w-100 h-100 py-2 border-0 bg-body" required />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {loading ?
+                        <tr>
+                            <td colSpan={2}>
+                                <div className="loading d-flex align-items-center justify-content-center rounded py-5">
+                                    <div className="text-center">
+                                        <div>
+                                            <div className="spinner-border text-primary"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        : persons.map(person => (
+                            <tr key={person.id}>
+                                <td className="p-2">{person.name}</td>
+                                <td className="p-2">{formatDate(person?.dob)}</td>
+                            </tr>
+                        ))}
+                    <tr>
+                        <td colSpan={2} className="p-2">
+                            <button className="btn btn-primary" type="submit">Search</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </form>
     )
 }
